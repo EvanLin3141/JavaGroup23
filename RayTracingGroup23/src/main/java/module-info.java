32df1e7175c5baa5e3e1419a5ff5c3737df04dfc
evan <evan.lin.3.141@gmail.com>
2024-11-11 13:20:31 +0000

    Updated Folder

--- RayTracingGroup23/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module raytracing {
-    requires java.desktop;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    exports cs3318.raytracing;
-    opens cs3318.raytracing to javafx.fxml;
-}
