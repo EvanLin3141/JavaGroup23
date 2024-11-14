25263ce81c72859ccf3bf82ec2f3cfdf86a7dc03
myra-n <myranwankwo@gmail.com>
2024-11-14 21:37:00 +0000

    added Javadoc comments.

--- src/main/java/lighting/Ray.java
+++ src/main/java/lighting/Ray.java
@@ -6,38 +6,72 @@ import render.Renderable;
 import javafx.scene.paint.Color;
 import java.util.List;
 
+/**
+ * Handles rays in a 3D space, including tracing the ray through objects in the scene
+ * and shading objects that the ray intersects
+ */
 public class Ray {
+
+    /** The maximum distance along a ray's path where an intersection is taken into account*/
     public static final float MAX_T = Float.MAX_VALUE;
+
     private Vector3D rayOrigin;
+
     private Vector3D rayDirection;
+
+    /** Used to determine where along the ray's path the intersection occurs */
     private float scalarParameter;
+
     private Renderable intersectedObject;
 
+
+    /**
+     * Constructs a new Ray object
+     * @param origin The origin of the ray
+     * @param direction The direction of the ray
+     */
     public Ray(Vector3D origin, Vector3D direction) {
         rayOrigin = new Vector3D(origin);
         rayDirection = Vector3D.normalize(direction);
     }
 
+    /** @return The origin of the ray */
     public Vector3D getOrigin() {
         return this.rayOrigin;
     }
 
+    /** @return The direction of the ray */
     public Vector3D getDirection() {
         return this.rayDirection;
     }
 
+    /** @return The scalar parameter of the ray */
     public float getScalarParam() {
        return this.scalarParameter;
     }
 
+
+    /**
+     * Sets the scalar parameter for the ray
+     * @param scalarParameter
+     */
     public void setScalarParam(float scalarParameter){
         this.scalarParameter = scalarParameter;
     }
 
+    /**
+     * Sets the object that the ray intersects
+     * @param object
+     */
     public void setIntersectedObject(Renderable object) {
         this.intersectedObject = object;
     }
 
+    /**
+     * Traces the ray through a list of objects to check for intersections
+     * @param objects The list of objects to check for intersection
+     * @return True if the ray intersects with an object, false if it doesn't
+     */
     public boolean trace(List<Object> objects) {
         this.scalarParameter = MAX_T;
         intersectedObject = null;
@@ -55,10 +89,19 @@ public class Ray {
     // to
     //            ray.object.Shade(ray, ...)
     //
+
+    /**
+     * Calculates the colour of the ray based on lighting and objects in  the scene
+     * @param lights The list of light sources in the scene
+     * @param objects The list of objects in the scene
+     * @param backgroundColour The background colour of the scene
+     * @return The colour of the ray after shading
+     */
     public final Color shade(List<Object> lights, List<Object> objects, Color backgroundColour) {
         return intersectedObject.shade(this, lights, objects, backgroundColour);
     }
 
+    /** @return A string representing the ray */
     public String toString() {
         return ("ray origin = "+rayOrigin+"  ray direction = "+rayDirection+"  t = "+scalarParameter);
     }
