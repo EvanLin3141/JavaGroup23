608f819b7d0ecafb2542e787e653a527170a04bb
myra-n <myranwankwo@gmail.com>
2024-11-16 00:28:58 +0000

    updated variable names.

--- src/main/java/shapes/Sphere.java
+++ src/main/java/shapes/Sphere.java
@@ -9,38 +9,38 @@ import java.util.List;
 
 // An example "Renderable" object
 public class Sphere implements Renderable {
-    Surface surface;
-    Vector3D center;
-    float radius;
-    float radSqr;
+    Surface sphereSurface;
+    Vector3D sphereCenter;
+    float sphereRadius;
+    float radiusSquared;
 
-    public Sphere(Surface s, Vector3D c, float r) {
-        surface = s;
-        center = c;
-        radius = r;
-        radSqr = r*r;
+    public Sphere(Surface surface, Vector3D center, float radius) {
+        sphereSurface = surface;
+        sphereCenter = center;
+        sphereRadius = radius;
+        radiusSquared = radius*radius;
     }
 
     @Override
     public boolean intersect(Ray ray) {
-        float dx = center.x - ray.getOrigin().x;
-        float dy = center.y - ray.getOrigin().y;
-        float dz = center.z - ray.getOrigin().z;
-        float v = ray.getDirection().dot(dx, dy, dz);
+        float dx = sphereCenter.x - ray.getOrigin().x;
+        float dy = sphereCenter.y - ray.getOrigin().y;
+        float dz = sphereCenter.z - ray.getOrigin().z;
+        float distanceToClosestPoint = ray.getDirection().dot(dx, dy, dz);
 
         // Do the following quick check to see if there is even a chance
         // that an intersection here might be closer than a previous one
-        if (v - radius > ray.getScalarParam())
+        if (distanceToClosestPoint - sphereRadius > ray.getScalarParam())
             return false;
 
         // Test if the ray actually intersects the sphere
-        float t = radSqr + v*v - dx*dx - dy*dy - dz*dz;
+        float t = radiusSquared + distanceToClosestPoint*distanceToClosestPoint - dx*dx - dy*dy - dz*dz;
         if (t < 0)
             return false;
 
         // Test if the intersection is in the positive
         // ray direction, and it is the closest so far
-        t = v - ((float) Math.sqrt(t));
+        t = distanceToClosestPoint - ((float) Math.sqrt(t));
         if ((t > ray.getScalarParam()) || (t < 0))
             return false;
 
@@ -49,7 +49,7 @@ public class Sphere implements Renderable {
         return true;
     }
     @Override
-    public Color shade(Ray ray, java.util.List<Object> lights, List<Object> objects, Color bgnd) {
+    public Color shade(Ray ray, java.util.List<Object> lights, List<Object> objects, Color backgroundColour) {
         // An object shader doesn't really do too much other than
         // supply a few critical bits of geometric information
         // for a surface shader. It must compute:
@@ -58,22 +58,22 @@ public class Sphere implements Renderable {
         //   2. a unit-length surface normal (n)
         //   3. a unit-length vector towards the ray's origin (v)
         //
-        float px = ray.getOrigin().x + ray.getScalarParam()*ray.getDirection().x;
-        float py = ray.getOrigin().y + ray.getScalarParam()*ray.getDirection().y;
-        float pz = ray.getOrigin().z + ray.getScalarParam()*ray.getDirection().z;
+        float intersectionX = ray.getOrigin().x + ray.getScalarParam()*ray.getDirection().x;
+        float intersectionY = ray.getOrigin().y + ray.getScalarParam()*ray.getDirection().y;
+        float intersectionZ = ray.getOrigin().z + ray.getScalarParam()*ray.getDirection().z;
 
-        Vector3D p = new Vector3D(px, py, pz);
-        Vector3D v = new Vector3D(-ray.getDirection().x, -ray.getDirection().y, -ray.getDirection().z);
-        Vector3D n = new Vector3D(px - center.x, py - center.y, pz - center.z);
-        n.normalize();
+        Vector3D intersectionPoint = new Vector3D(intersectionX, intersectionY, intersectionZ);
+        Vector3D viewVector = new Vector3D(-ray.getDirection().x, -ray.getDirection().y, -ray.getDirection().z);
+        Vector3D surfaceNormal = new Vector3D(intersectionX - sphereCenter.x, intersectionY - sphereCenter.y, intersectionZ - sphereCenter.z);
+        surfaceNormal.normalize();
 
         // The illumination model is applied
         // by the surface's Shade() method
-        return surface.shade(p, n, v, lights, objects, bgnd);
+        return sphereSurface.shade(intersectionPoint, surfaceNormal, viewVector, lights, objects, backgroundColour);
     }
 
     @Override
     public String toString() {
-        return ("sphere "+center+" "+radius);
+        return ("sphere "+sphereCenter+" "+sphereRadius);
     }
 }
